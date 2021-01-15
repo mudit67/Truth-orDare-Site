9c54c863766e74e065792da1e0e67b594cf10b02
mudit67 <bhardwajmudit006@gmail.com>
2021-01-15 15:44:18 +0530

    netlify form integration try 3

--- src/App.js
+++ src/App.js
@@ -52,7 +52,7 @@ class App extends React.Component{
             Truth or Dare
           </h1>
         </nav>
-        <form data-netlify="true" name="users" onSubmit={(e) => {this.appendName();e.preventDefault();}}>
+        <form data-netlify="true" name="users" onSubmit={(e) => {this.appendName();e.preventDefault();}} netlify>
           <div className="input-container">
             <input name="user" value={this.state.user || ""} type="name" onChange={this.changeHandler} placeholder="Enter a player name."/>
             <div className="input-bar"/>
